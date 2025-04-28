d2c5597ffa185997bcd0f3d81a490e2d34454fb4
Ahmed Gamal <ahmedmgh67@gmail.com>
2025-04-28 12:48:58 +0300

    ui cosmetics

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QSplashScreen *splash = new QSplashScreen;
-    splash->setPixmap(QPixmap("splash.png"));
+    splash->setPixmap(QPixmap("images/splash.png"));
     splash->show();
 
     MainWindow w;
